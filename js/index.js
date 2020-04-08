3dabb762605be7a8042af22f6bf5daef241d406f
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-04-08 14:24:50 -0700

    added alert for error message but doesn't work yet

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-    
     $('#search-button').click(getData);
 
     function getData(event){
         event.preventDefault();
         var input = encodeURIComponent($('#search-input').val().trim())   
-        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=5hAFzhlPwnIpT4XY2vkt69nnzDYTE530&limit=30");
+        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=5hAFzhlPwnIpT4XY2vkt69nnzDYTE530&limit=30")
         xhr.done(function(response) {
-            console.log('success, got data!', response);
             var gifs = response.data
             $('#gifs').empty();
             for (gif of gifs){
                 $('#gifs').append("<img src='"+gif.images.original.url+"'>");
             };
+        xhr.fail(function(error) {
+            console.log('error', error);
+            alert('An error occurred, please try again later');
+        })
     });
     };
 });
