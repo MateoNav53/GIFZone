e27e31ea6f90d79d7b66ab5f96273546fbc4959a
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-04-09 14:16:20 -0700

    made gifs clickable and added .fail function

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
     $('#search-button').click(getData);
-
     function getData(event){
         event.preventDefault();
         var input = encodeURIComponent($('#search-input').val().trim())   
-        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=5hAFzhlPwnIpT4XY2vkt69nnzDYTE530&limit=30")
+        var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q='${input}'&api_key=5hAFzhlPwnIpT4XY2vkt69nnzDYTE530&limit=30`)
         xhr.done(function(response) {
             var gifs = response.data
             $('#gifs').empty();
             for (gif of gifs){
-                $('#gifs').append("<img src='"+gif.images.original.url+"'>");
+                $('#gifs').append(`<a href='${gif.images.original.url}' download><img src='${gif.images.original.url}'></a>`);
             };
-        xhr.fail(function(error) {
-            console.log('error', error);
+        });
+        xhr.fail(function() {
             alert('An error occurred, please try again later');
-        })
-    });
+        });
     };
 });
